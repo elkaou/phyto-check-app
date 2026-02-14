815ef65f87356e24d1a5af78bcc7deb0477f033e
elkaou <francois@siteswebs.fr>
2026-02-14 15:26:02 +0100

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -96,6 +96,7 @@ const config: ExpoConfig = {
           enableProguardObfuscation: true,
           extraMavenRepos: ["https://maven.google.com"],
           gradleProperties: {
+            "react.native.iap.store": "play",
             "com.google.android.play.billingclient.version": "6.0.1",
           },
         },
